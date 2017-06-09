01217a43886362f6bb5cf796cad025574be5c568
GuaiYiHu <guaiyihu@foxmail.com>
2017-06-10 01:48:16 +0800

    add root
    Change-Id: I6b6612518415a7e8a4d1cd7a363f63f213637176

--- releasetools.py
+++ releasetools.py
@@ -14,6 +14,19 @@ ui_print("* Now loading Flyme...");
 ui_print("******************************************");
 mount("ext4", "EMMC", "/dev/block/platform/sdhci-tegra.3/by-name/userdata", "/data", "");\n''' + edify.script[0]
 
+def InstallSuperSU(info):
+    root = info.input_zip.read("META/UPDATE-SuperSU.zip")
+    common.ZipWriteStr(info.output_zip, "SuperSU/UPDATE-SuperSU.zip", root)
+
+def FlashSUperSU(info):
+    info.script.AppendExtra(('ui_print("Flashing SuperSU...");'))
+    info.script.AppendExtra(('package_extract_dir("SuperSU", "/tmp/supersu");'))
+    info.script.AppendExtra(('run_program("/sbin/busybox", "unzip", "/tmp/supersu/UPDATE-SuperSU.zip", "META-INF/com/google/android/*", "-d", "/tmp/supersu");'))
+    info.script.AppendExtra(('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/supersu/UPDATE-SuperSU.zip");'))
+    info.script.AppendExtra(('ui_print("Finish!");'))
+
 def FullOTA_InstallEnd(info):
     edify = info.script
     ModifyBegin(edify)
+    InstallSuperSU(info)
+    FlashSUperSU(info)
